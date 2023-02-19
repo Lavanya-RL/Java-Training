34de27997095261c8afe6b9559986e97ca498d04
LavanayRaman <lavanya.raman@cdw.com>
2023-02-19 23:57:14 +0530

    modifies  9

--- excercise9.java
+++ excercise9.java
@@ -1,5 +1,25 @@
-package day2;
-
-public class excercise9 {
-
+ppackage day2;
+ class A {
+    public  int hrs=10;
+    private int num;
+    protected int id;
+    public void method1() {
+    	
+    	
+    }
 }
+ class b extends A{
+	 void sal() {
+	
+	int salary=hrs*id;
+	System.out.println(salary);
+}}
+public class excercise9{
+	public static void main(String[] args) {
+		b obj=new b();
+		 obj.sal();
+		
+	}
+}
+// cnnot acces privatre ,but can acess public and protected in the same 
+//package
