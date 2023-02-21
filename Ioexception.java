d4d064fe7949c570a6b2c68d8421e40b55fa333c
LavanayRaman <lavanya.raman@cdw.com>
2023-02-21 19:48:41 +0530

    Create Ioexception.java

---
+++ Ioexception.java
@@ -0,0 +1,22 @@
+package exceptionhandling;
+
+package exceptionhandling;
+import java.io.IOException;
+import java.util.*;
+public class Ioexception {
+     public static void main(String[] args) {
+    	 Scanner scanner=new Scanner(System.in);
+    	 System.out.println("Please enter  item...:");
+			String type=scanner.next();
+		
+	}
+}
+class Tree{
+public void Branch(String type) {
+	if(type.equals("soft")) {
+		throw  new IOException("Do not type soft");
+		
+	}
+	// error should add throw declaration 
+}
+}
