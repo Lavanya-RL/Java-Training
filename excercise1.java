34541aa9006b9bb6caeec0d695b2e94dd161ec89
LavanayRaman <lavanya.raman@cdw.com>
2023-02-21 23:23:29 +0530

    Update excercise1.java

--- excercise1.java
+++ excercise1.java
@@ -2,18 +2,18 @@ package day2;
 
 public class excercise1 {
 	int a=10;
-	void call(int a) {
+	void callbyvalue(int a) {
 	 a=a+10;
 	}
-	void callref(excercise1 eg) {
+	void callbyreference(excercise1 eg) {
 		eg.a=eg.a +10;
 	}
 	public static void main(String[] args) {
 		excercise1 ex=new excercise1();
 		
-		ex.call(10);
+		ex.callbyvalue(10);
 		System.out.println("after pass by value"+ex.a);
-         ex.callref(ex);
+         ex.callbyreference(ex);
          System.out.println("after pass by reference"+ex.a);
 }
 }
