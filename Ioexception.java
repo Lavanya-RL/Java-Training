4a096b6097235fb0b149b4c96a9172eccaf0e35e
LavanayRaman <lavanya.raman@cdw.com>
2023-02-23 19:01:32 +0530

    MODIFIED AS PER REVIEW

--- Ioexception.java
+++ Ioexception.java
@@ -1,22 +1,20 @@
-package exceptionhandling1;
-
 package exceptionhandling;
 import java.io.IOException;
 import java.util.*;
 public class Ioexception {
-     public static void main(String[] args) {
+     public static void main(String[] args) throws Exception {
     	 Scanner scanner=new Scanner(System.in);
     	 System.out.println("Please enter  item...:");
 			String type=scanner.next();
+			Tree tree=new Tree();
+			tree.Branch(type);
 		
 	}
 }
 class Tree{
-public void Branch(String type) {
-	if(type.equals("soft")) {
-		throw  new IOException("Do not type soft");
+public void Branch(String type) throws Exception {
+	System.out.println(tree);
+}
+}
+	
 		
-	}
-	// error should add throw declaration 
-}
-}
